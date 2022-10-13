7f369b77503ecf7d0098e708c34bf5a92f5c6e0f
Jung-Han Han <hjhcw@naver.com>
2022-10-12 18:50:46 -0700

    Made the received contact stored in DB after validating it.

--- controllers/contact-api-controller.js
+++ controllers/contact-api-controller.js
@@ -1,32 +1,51 @@
 import nodeMailer from 'nodemailer';
+import mongoose from 'mongoose';
 
-const sendEmail = (req, res) => {
-  const { firstName, lastName, email, message, sendCopy } = req.body;
-  // console.log(`${firstName}, ${lastName}, ${email}, ${message}, ${sendCopy}`);
+const contactSchema = mongoose.model("contact");
 
-  const mailData = {
-    from: email,
-    to: process.env.EMAIL,
-    subject: `Received message from ${firstName + " " + lastName}, email:${email}.`,
-    text: `Received message from ${firstName + " " + lastName}, email:${email}\n\n${message}`
-  };  
+const sendEmail = async (req, res) => {
+  try {
+    // store the contact in DB
+    // if the received contact is not validated by the schema, 
+    // it will throw an exception, and thus not storing the contact
+    // as well as not sending an email.
+    let {firstName, lastName, email, message, sendCopy} = await contactSchema.create(req.body);
+    
+    const mailData = {
+      from: email,
+      to: process.env.EMAIL,
+      subject: `Received message from ${firstName + " " + lastName}, email:${email}.`,
+      text: `Received message from ${firstName + " " + lastName}, email:${email}\n\n${message}`
+    };
 
-  const transporter = nodeMailer.createTransport({
-    port: 465,
-    host: process.env.HOST,
-    auth: {
-      user: process.env.EMAIL,
-      pass: process.env.PASSWORD
-    },
-    secure: true
-  });
+    const transporter = nodeMailer.createTransport({
+      port: 465,
+      host: process.env.HOST,
+      auth: {
+        user: process.env.EMAIL,
+        pass: process.env.PASSWORD
+      },
+      secure: true
+    });
 
-  transporter.sendMail(mailData, (error, info) => {
-    if(error) {
-      return console.log(error);
-    }
-    res.status(200).send("Mail sent");
-  });  
+    transporter.sendMail(mailData, (error, info) => {
+      if (error) {
+        throw error;
+      }
+      console.log("Mail sent");
+      console.log(info);
+      res.status(200).send("Mail sent");
+    });
+  }
+  catch (err) {
+    console.error(err);
+    res
+      .status(400)
+      .send(
+        "Bad Request. The message in the body of the \
+        Request is either missing or malformed."
+      );
+  }
 };
 
 export { sendEmail };
