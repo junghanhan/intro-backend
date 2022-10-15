204f627beb7bf802764ce925a0e7608f1df5a8a1
Jung-Han Han <hjhcw@naver.com>
2022-10-15 12:36:29 -0700

    Changed the order of retrieved data from DB. (descending ID to ascending ID)

--- controllers/project-api-controller.js
+++ controllers/project-api-controller.js
@@ -5,7 +5,7 @@ const projectModel = mongoose.model('project');
 
 const getAllProjects = async (req, res) => {
   try {
-    let projects = await projectModel.find({}, '', {sort: {_id: -1}}).exec();
+    let projects = await projectModel.find({}, '', {sort: {_id: 1}}).exec();
     res.status(200).json(projects);
   } catch(err) {
     res.status(400).send("Bad Request");
--- controllers/skill-api-controller.js
+++ controllers/skill-api-controller.js
@@ -5,7 +5,7 @@ const skillModel = mongoose.model('skill');
 
 const getAllSkills = async (req, res) => {
   try {
-    let skills = await skillModel.find({}, '', {sort: {_id: -1}}).exec();
+    let skills = await skillModel.find({}, '', {sort: {_id: 1}}).exec();
     res.status(200).json(skills);
   } catch(err) {
     res.status(400).send("Bad Request");
